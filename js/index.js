b7d23fda415427c8f33f077fa27842b14b025371
Jordan Degner <jdegner0129@gmail.com>
2014-02-13 10:26:33 -0600

    Implemented per-page Disqus and used the main page for excerpts.

--- js/index.js
+++ js/index.js
@@ -1,13 +1,3 @@
 $(function() {
 	$('#posts-nav').addClass('active');
-
-	/* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
-    var disqus_shortname = 'jdegnerproject52'; // Required - Replace example with your forum shortname
-
-    /* * * DON'T EDIT BELOW THIS LINE * * */
-    (function() {
-        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-    })();
 });
--- js/post.js
+++ js/post.js
@@ -1,3 +1,15 @@
 $(function() {
 	$('#posts-nav').addClass('active');
+
+	var url = location.protocol + '//' + location.host + location.pathname;
+	/* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+    var disqus_shortname = 'jdegnerproject52'; // Required - Replace example with your forum shortname
+    var disqus_url = url;
+
+    /* * * DON'T EDIT BELOW THIS LINE * * */
+    (function() {
+        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    })();
 });
